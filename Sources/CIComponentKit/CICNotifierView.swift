34ae286e9cd867c217fe56f4252a932ee736c5d1
ManoBoo <manoboo@icloud.com>
2018-01-02 15:03:38 +0800

    fix CICNotifierViewType `hashValue` bug

--- Sources/CIComponentKit/CICNotifierView.swift
+++ Sources/CIComponentKit/CICNotifierView.swift
@@ -36,7 +36,7 @@ public struct CICNotifierViewType: Hashable {
     }
 
     public var hashValue: Int {
-        return (imageName + String.cic.random()).hashValue
+        return imageName.hashValue + textColor.hashValue
     }
 
     public static func == (lhs: CICNotifierViewType, rhs: CICNotifierViewType) -> Bool {
@@ -45,11 +45,11 @@ public struct CICNotifierViewType: Hashable {
 }
 
 class CICNotifierView: CICUIView {
-    
+
     private let imageView = UIImageView().size(20.0.makeSize)
     private let titleLabel = CICLabel().font(UIFont.cic.preferred(.body)).line(0)
     private let closeBtn = UIButton().size(20.0.makeSize)
-    
+
     convenience init(type: CICNotifierViewType = .status,
                      title: String = "",
                      isShowClose: Bool = true,
@@ -59,7 +59,7 @@ class CICNotifierView: CICUIView {
         self.layer.shadowColor = type.color.cgColor
         self.layer.cornerRadius = 10.0
         initSubviews()
-        
+
         switch (type) {
         case .error, .status, .success, .warning:
             imageView.image = UIImage.cic.bundle(type.imageName)
@@ -74,36 +74,83 @@ class CICNotifierView: CICUIView {
             closeBtn.addHandler(for: .touchUpInside, handler: closeHandler)
         }
     }
-    
+
     func initSubviews() {
         addSubview(imageView)
         addSubview(titleLabel)
         addSubview(closeBtn)
     }
-    
+
     func render() {
         imageView.x(20).centerY(self.cic.internalCenterY)
         closeBtn.right(20.0).centerY(self.cic.internalCenterY)
-        let maxTitleWidth = self.cic.width - imageView.cic.right - closeBtn.cic.x - 20
-        titleLabel.x(imageView.cic.right + 20).width(maxTitleWidth).sizeTo(layout: .maxWidth(maxTitleWidth))
+        let maxTitleWidth = closeBtn.cic.x - imageView.cic.right - 20
+        titleLabel.x(imageView.cic.right + 20).width(maxTitleWidth)
+            .sizeTo(layout: .maxWidth(maxTitleWidth)).centerY(self.cic.internalCenterY)
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+
         render()
     }
-    
+
     override func sizeThatFits(_ size: CGSize) -> CGSize {
         self.width(size.width)
         render()
         return CGSize.init(width: size.width, height: titleLabel.cic.height + 22)
     }
-    
+
     override func deviceOrientationDidChange() {
         super.deviceOrientationDidChange()
-        
+
         sizeToFit()
+    }
+}
+
+extension CICHUD {
+
+    /// 弹出一个通知窗体
+    ///
+    /// - Parameters:
+    ///   - type: CICNotifierViewType, 可自定义 CICNotifierViewType.init(....)
+    ///   - title: 展示的文本
+    ///   - offSet: 距离屏幕上方的offSet
+    ///   - isShowClose: 是否展示右侧关闭按钮
+    ///   - autoHide: 是否自动隐藏
+    ///   - duration: autoHide为true时,弹出和隐藏的动画时间
+    public class func showNotifier(_ type: CICNotifierViewType = .status,
+                                   title: String,
+                                   offSet: CGFloat? = nil,
+                                   leftOffSet: CGFloat = 10,
+                                   isShowClose: Bool = true,
+                                   autoHide: Bool = true,
+                                   duration: TimeInterval = 2.0,
+                                   closeHandler: ((UIControl) -> Void)? = nil) {
+        guard let keyWindow = UIApplication.shared.keyWindow else {
+            return
+        }
+
+        let notifier = CICNotifierView.init(type: type,
+                                                title: title,
+                                                isShowClose: isShowClose,
+                                                closeHandler: closeHandler)
+        notifier.x(10).width(CGFloat.screenWidth - 20).sizeToFit()
+        notifier.y(-notifier.cic.height)
+        let top = offSet ?? CGFloat.maximum(keyWindow.layoutMargins.top, 20)
+        keyWindow.addSubview(notifier)
+        UIView.cic.spring ({
+            notifier.y(top)
+        })
+        if autoHide {
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: {
+                UIView.cic.spring({
+                    notifier.y(-notifier.cic.height)
+                }, completion: {
+                    notifier.removeFromSuperview()
+                })
+            })
+        }
     }
 }
 
@@ -177,49 +224,50 @@ class CICNotifierViewLayout: SizeLayout<View> {
     }
 }
 
-extension CICHUD {
+//extension CICHUD {
+//
+//    /// 弹出一个通知窗体
+//    ///
+//    /// - Parameters:
+//    ///   - type: CICNotifierViewType, 可自定义 CICNotifierViewType.init(....)
+//    ///   - title: 展示的文本
+//    ///   - offSet: 距离屏幕上方的offSet
+//    ///   - isShowClose: 是否展示右侧关闭按钮
+//    ///   - autoHide: 是否自动隐藏
+//    ///   - duration: autoHide为true时,弹出和隐藏的动画时间
+//    public class func showNotifier(_ type: CICNotifierViewType = .status,
+//                                   title: String,
+//                                   offSet: CGFloat? = nil,
+//                                   leftOffSet: CGFloat = 10,
+//                                   isShowClose: Bool = true,
+//                                   autoHide: Bool = true,
+//                                   duration: TimeInterval = 2.0) {
+//        guard let keyWindow = UIApplication.shared.keyWindow else {
+//            return
+//        }
+//        let randomid = String.cic.random()
+//        let notifierLayout = CICNotifierViewLayout.init(type: type,
+//                                                        title: title,
+//                                                        isShowClose: isShowClose,
+//                                                        viewReuseId: randomid)
+//        let top = offSet ?? CGFloat.maximum(keyWindow.layoutMargins.top, 20)
+//        let height = notifierLayout.arrangement(width: CGFloat.screenWidth - 20).frame.height
+//        let notifier = notifierLayout.arrangement(origin: CGPoint.init(x: leftOffSet, y: -height),
+//                                                  width: CGFloat.screenWidth - 2 * leftOffSet,
+//                                                  height: nil).makeViews()
+//        keyWindow.addSubview(notifier)
+//        UIView.cic.spring ({
+//            notifier.y(top)
+//        })
+//        if autoHide {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: {
+//                UIView.cic.spring({
+//                    notifier.y(-notifier.cic.height)
+//                }, completion: {
+//                    notifier.removeFromSuperview()
+//                })
+//            })
+//        }
+//    }
+//}
 
-    /// 弹出一个通知窗体
-    ///
-    /// - Parameters:
-    ///   - type: CICNotifierViewType, 可自定义 CICNotifierViewType.init(....)
-    ///   - title: 展示的文本
-    ///   - offSet: 距离屏幕上方的offSet
-    ///   - isShowClose: 是否展示右侧关闭按钮
-    ///   - autoHide: 是否自动隐藏
-    ///   - duration: autoHide为true时,弹出和隐藏的动画时间
-    public class func showNotifier(_ type: CICNotifierViewType = .status,
-                                   title: String,
-                                   offSet: CGFloat? = nil,
-                                   leftOffSet: CGFloat = 10,
-                                   isShowClose: Bool = true,
-                                   autoHide: Bool = true,
-                                   duration: TimeInterval = 2.0) {
-        guard let keyWindow = UIApplication.shared.keyWindow else {
-            return
-        }
-        let randomid = String.cic.random()
-        let notifierLayout = CICNotifierViewLayout.init(type: type,
-                                                        title: title,
-                                                        isShowClose: isShowClose,
-                                                        viewReuseId: randomid)
-        let top = offSet ?? CGFloat.maximum(keyWindow.layoutMargins.top, 20)
-        let height = notifierLayout.arrangement(width: CGFloat.screenWidth - 20).frame.height
-        let notifier = notifierLayout.arrangement(origin: CGPoint.init(x: leftOffSet, y: -height),
-                                                  width: CGFloat.screenWidth - 2 * leftOffSet,
-                                                  height: nil).makeViews()
-        keyWindow.addSubview(notifier)
-        UIView.cic.spring ({
-            notifier.y(top)
-        })
-        if autoHide {
-            DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: {
-                UIView.cic.spring({
-                    notifier.y(-notifier.cic.height)
-                }, completion: {
-                    notifier.removeFromSuperview()
-                })
-            })
-        }
-    }
-}
